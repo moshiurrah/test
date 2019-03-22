06ff282a8753a0d2acf5e5c5424feff4fcd72ef9
Mohammad Moshiur Rahman <43156585+moshiurrah@users.noreply.github.com>
2019-03-22 15:13:17 -0400

    Update itemActions.js

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, UPDATE_ITEM,ITEMS_LOADING } from './types';
 import { tokenConfig } from './authActions';
 import { returnErrors } from './errorActions';
 
